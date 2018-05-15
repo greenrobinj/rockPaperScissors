7ecc4da2d001fed44fe791e03e5684c035b1222e
Robin Green <greenrj4@gmail.com>
2018-05-15 09:27:49 -0600

    not much better

--- rock.js
+++ rock.js
@@ -1,8 +1,55 @@
+
+/* in JavaScript  ver is used to set s veryable  (var blank = thing)
+in ruby you could just put blank is equel to  thing ( blank = thing)
+
+evey thing really comes from 'object.prototype'
+
+*/
+
+var longString = "this is a long string"
+var shortString = 'shorter'
+
+console.log(
+  longString.length()/* need to have the () to make the function
+  even if its nothing   other wise it is a  property
+
+
+
 /* ROCK PAPER SISIONRS
 USER SELECTION.
 COMPUTER SELETION
 COMPAIR THE TO 
 DECIDE WINNER*/
 
-<button onclick = { ROCK }
-  
+var choice1 = rock
+var choice2 = paper 
+var choice3 = sissors
+
+
+
+
+switch(myChoice){
+  case "rock":
+    myChoice.addEventListener(onclick){
+    myChoice == choice1
+  case "paper":
+  yChoice.addEventListener(onclick)
+    myChoice == choice2
+  default   
+    myChoice == scissors
+}
+
+
+function myChoice (){
+  myChoice = onclick 
+}
+
+var game = [
+  "rock", 
+  "paper", 
+  "sicssors"
+);
+
+var game.random = compPlay
+
+
